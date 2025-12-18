6af63a053d0933f40e3d7de56e71c81e5c3450b1
Marcos Willian <marcoswms@gmail.com>
2025-12-18 19:02:41 -0300

    add: atividade Factory Method - SimpleFactory

---
+++ creational-patterns-exercicio/src/main/java/br/com/cod3r/factory/apple/factory/IPhoneSimpleFactory.java
@@ -0,0 +1,31 @@
+package br.com.cod3r.factory.apple.factory;
+
+import br.com.cod3r.factory.apple.model.*;
+
+public class IPhoneSimpleFactory {
+
+    public static IPhone orderIphone(String generation, String level) {
+        IPhone device = null;
+
+        if ("X".equals(generation)) {
+            if ("standard".equals(level)) {
+                device = new IPhoneX();
+            } else if ("highEnd".equals(level)) {
+                device = new IPhoneXSMax();
+                device.getHardware();
+            }
+        } else if ("11".equals(generation)) {
+            if ("standard".equals(level)) {
+                device = new IPhone11();
+            } else if ("highEnd".equals(level)) {
+                device = new IPhone11Pro();
+            }
+        }
+
+        device.getHardware();
+        device.assemble();
+        device.certificates();
+        device.pack();
+        return device;
+    }
+}
